30121d7cbbd4bf8bff91d2c510ac0041b21daafd
Mayank Mokta <mayankmokta@gmail.com>
2025-10-22 17:46:34 +0530

    scripts updated

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+
+contract HelperConfig is Script {
+
+    error HelperConfig__InvalidChainId();
+
+        struct NetworkConfig {
+            address entryPoint;
+            address account;
+        }
+        NetworkConfig public localNetworkConfig;
+
+
+        mapping(uint256 chainid => NetworkConfig) public NetworkConfigs;
+        uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+        uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
+        uint256 constant LOCAL_CHAIN_ID = 31337;
+        address constant BURNER_WALLET = 0x7eb5b82F7754CAd509a8d13fB4092E1Fc77Ee951;
+
+
+        constructor() {
+            NetworkConfigs[11155111] = getEthSepoliaConfig();
+        }
+
+
+
+        function getConfig() public returns (NetworkConfig memory){
+            return getConfigByChainId(block.chainid);
+        }
+
+
+
+        function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory){
+            if(chainId == LOCAL_CHAIN_ID){
+                return getOrCreateAnvilEthConfig();
+        }
+        else if(NetworkConfigs[chainId].account != address(0)){
+            return NetworkConfigs[chainId];
+        }else{
+            revert HelperConfig__InvalidChainId();
+        }
+        }
+
+
+        function getEthSepoliaConfig() public pure returns(NetworkConfig memory){
+            return NetworkConfig({
+                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+                account: BURNER_WALLET
+            });
+        }
+
+
+
+        function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory){
+            return NetworkConfig({
+                entryPoint: address(0),
+                account: BURNER_WALLET
+            });
+        }
+
+
+
+        function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory){
+            if(localNetworkConfig.account != address(0)){
+                return localNetworkConfig;
+            }
+            return NetworkConfig({
+                entryPoint: address(0),
+                account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
+            });
+        }
+
+        
+
+    }
+
